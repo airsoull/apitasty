aa6151b61e9e3f588880522883ec58e6412ae0b2
Cristian Sepulveda <air.enrill@gmail.com>
2015-03-21 00:04:14 -0300

    se manda True a la API

--- api/api.py
+++ api/api.py
@@ -1,19 +1,20 @@
 import json
 
 from django.http import HttpResponse
-
+from django.contrib.auth.models import User
+# from tastypie.resources import ModelResource
 from tastypie.resources import ModelResource
 
-
 class UserResource(ModelResource):
+    
     class Meta:
+        queryset = User.objects.all()
         resource_name = 'user'
-        detail_allowed_methods = ['post', 'get',]
+        # detail_allowed_methods = ['post', 'get',]
         always_return_data = True
         default_format = 'application/json'
 
-    	def get_list(self, request, **kwargs):
-	        super(UserResource, self).get_list(request, **kwargs)
-	        data = {'value': True}
-	        return HttpResponse(mimetype='application/json; charset=utf-8', content=json.dumps(data), status=200)
+    def dehydrate(self, bundle):
+        bundle.data['value'] = True
+        return bundle
 
