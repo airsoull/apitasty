062e3cfc09ed656ff598b451d16c762cc968d4c6
Cristian Sepulveda <air.enrill@gmail.com>
2015-03-22 17:57:15 -0300

    cambio js para leer api

--- tasty/static/js/find_friend.js
+++ tasty/static/js/find_friend.js
@@ -9,9 +9,11 @@ function even(e){
     $url = 'http://tasty.herokuapp.com/api/facebook/friend/?format=json';
     $url = $url + '&email=' + $email + '&email_friend=' + $email_friend;
 
-    $.get($url, function(data){
+    $.get($url, function(data, textStatus, xhr){
     }).success(function(data){
       console.log(data);
+    }).fail(function(data, textStatus, xhr){
+      console.log(xhr.status);
     });
 
   });
