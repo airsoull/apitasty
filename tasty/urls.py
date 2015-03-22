255f4a4deeff8472218e29647ccffecad8cb487d
Cristian Sepulveda <air.enrill@gmail.com>
2015-03-21 22:51:19 -0300

    Se obtienen amigos de FB y se compara si son amigos

--- tasty/urls.py
+++ tasty/urls.py
@@ -5,7 +5,7 @@ from api.api import UserResource
 user_resource = UserResource()
 
 urlpatterns = patterns('',
-	url(r'^api/(?P<email>[\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})/(?P<fiend_email>[\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})/', include(user_resource.urls)),
+	url(r'^api/', include(user_resource.urls)),
     url('', include('social.apps.django_app.urls', namespace='social')),
     url(r'^admin/', include(admin.site.urls)),
 )
