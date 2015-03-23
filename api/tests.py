a49e0b2bd74055a1153dc70360f3f1f3839e0fe1
Cristian Sepulveda <air.enrill@gmail.com>
2015-03-23 16:17:14 -0300

    se crean primeros test para la API

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from tastypie.test import ResourceTestCase
 
-# Create your tests here.
+
+class UserResourceTest(ResourceTestCase):
+
+	def setUp(self):
+		self.url = '/api/facebook/friend/'
+
+	def test_get_without_parameters(self):
+		response = self.client.get(self.url, format='json')
+		self.assertEqual(404, response.status_code)
+
+	def test_get_only_email_parameter(self):
+		response = self.client.get(self.url, {'email': 'your_email@email.com'}, format='json')
+		self.assertEqual(404, response.status_code)
+
+	def test_get_only_email_friend_parameter(self):
+		response = self.client.get(self.url, {'email_friend': 'email_friend@email.com'}, format='json')
+		self.assertEqual(404, response.status_code)
+
+	def test_get_with_both_parameters_user_no_exist(self):
+		response = self.client.get(self.url, {'email': 'your_email@email.com', 'email_friend': 'email_friend@email.com'}, format='json')
+		self.assertEqual(404, response.status_code)
